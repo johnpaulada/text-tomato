2d04a32b095e1b738e3bc69c39b8fdc09defdbb1
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2018-01-31 09:01:44 +0800

    Complete functionality

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ import { h, app } from "hyperapp"
 /** @jsx h */
 
 const toWords = number => {
-  let words = ""
-  let power = ""
   const wordIndex = {
+    0: 'zero',
     1: 'one',
     2: 'two',
     3: 'three',
@@ -14,12 +13,29 @@ const toWords = number => {
     7: 'seven',
     8: 'eight',
     9: 'nine',
-    10: 'ten'
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'fourty',
+    50: 'fifty'
   }
 
-  while (number > 0) {
-    const digit = number % 10;
-    number = Math.floor(number / 10)
+  if (number > 19) {
+    const ones = number % 10
+    const tens = Math.floor(number / 10)
+
+    return `${wordIndex[tens * 10]} ${ones === 0 ? '' : wordIndex[ones]}`
+  } else {
+    return wordIndex[number]
   }
 }
 
@@ -27,43 +43,61 @@ const state = {
   remaining: 25,
   focus: 25,
   rest: 5,
-  mode: 'focus'
+  mode: 'focus',
+  timer: null
 }
 
 const actions = {
   plusFocus: () => state => ({focus: state.focus + 1}),
   minusFocus: () => state => ({focus: state.focus === 1 ? 1 : state.focus - 1}),
-  plusRest: () => state => ({focus: state.rest + 1}),
-  minusRest: () => state => ({focus: state.rest === 1 ? 1 : state.rest - 1}),
+  plusRest: () => state => ({rest: state.rest + 1}),
+  minusRest: () => state => ({rest: state.rest === 1 ? 1 : state.rest - 1}),
   decrement: () => state => ({remaining: state.remaining - 1}),
-  start: () => (state, actions) => {
+  startFocus: () => (state, actions) => {
     const timer = new Timer({
       tick: 60,
-      ontick: ms => {
-        actions.decrement()
-      }
+      ontick: () => actions.decrement(), 
+      onend: () => actions.startBreak()
     })
     
     timer.start(60*state.focus)
 
-    return {remaining: state.focus}
+    return {timer, remaining: state.focus, mode: 'focus'}
+  },
+  startBreak: () => (state, actions) => {
+    const timer = new Timer({
+      tick: 60,
+      ontick: () => actions.decrement(), 
+      onend: () => actions.startFocus()
+    })
+    
+    timer.start(60*state.rest)
+
+    return {timer, remaining: state.focus, mode: 'break'}
+  },
+  reset: () => (state, actions) => {
+    if (state.timer) {
+      state.timer.stop()
+    }
+
+    return {timer: null, remaining: 25, focus: 25, rest: 5, mode: 'focus'}
   }
 }
 
 const view = (state, actions) => (
   <div class="container">
     <div class="display">
-      <h2 class="display__time display--text">{state.remaining}</h2>
+      <h2 class="display__time display--text">{toWords(state.remaining)}</h2>
       <h3 class="display__details display--text">MINUTES LEFT ({state.mode})</h3>
     </div>
     <div class="options">
       <div class="options__box">
-        <h3 class="options__option focus">{state.focus}</h3>
-        <h4 class="options__description">FOCUS | <span onclick={actions.plusFocus}>plus</span> | <span onclick={actions.minusFocus}>minus</span> | <span onclick={actions.start}>start</span></h4>
+        <h3 class="options__option focus">{toWords(state.focus)}</h3>
+        <h4 class="options__description">FOCUS | <span onclick={actions.plusFocus}>plus</span> | <span onclick={actions.minusFocus}>minus</span> | <span onclick={actions.startFocus}>start</span> | <span onclick={actions.reset}>reset</span></h4>
       </div>
       <div class="options__box">
-        <h3 class="options__option rest">{state.rest}</h3>
-        <h4 class="options__description">REST | <span onclick={actions.plusRest}>plus</span> | <span onclick={actions.minusRest}>minus</span> | <span onclick={actions.start}>start</span></h4>
+        <h3 class="options__option rest">{toWords(state.rest)}</h3>
+        <h4 class="options__description">BREAK | <span onclick={actions.plusRest}>plus</span> | <span onclick={actions.minusRest}>minus</span> | <span onclick={actions.startBreak}>start</span> | <span onclick={actions.reset}>reset</span></h4>
       </div>
     </div>
   </div>
